40bbfa853a9df24fb9e94a26a500222dd449499a
Rishabhsharma2211 <rishabhcse2006@gmail.com>
2026-04-23 11:58:16 +0530

    Update UI

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://lost-found-mern-vq58.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
